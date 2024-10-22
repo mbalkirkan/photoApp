64355cf19a5d86eb3974c35676fa814bc94696ed
mbalkirkan <hbmemati@gmail.com>
2024-10-22 12:43:04 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\PhotoController::class, 'get']);
 Route::post('/', [\App\Http\Controllers\PhotoController::class, 'upload']);
 
-Route::get('/photos/last-merged', [PhotoController::class, 'getLastPhotoMerged']);
+Route::get('/photos/last-merged', [PhotoController::class, 'getMergedPhoto']);
